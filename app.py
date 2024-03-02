1f35196d3eb5da0641eb5977952ce5d047066a95
sally <sallypark73@hotmail.com>
2024-03-02 11:55:59 -0800

    init UI

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import subprocess
+import platform
+
+# st.write("Hello world")
+
+# df = pd.DataFrame({
+#   'first column': [1, 2, 3, 4],
+#   'second column': [10, 20, 30, 40]
+# })
+
+# df
+
+#=============================================================================
+# Upload Dataset Section
+#=============================================================================
+st.subheader("Upload Dataset")
+# Display file input button
+uploaded_file = st.file_uploader("Choose a file")
+
+# Display filename if a file is uploaded
+if uploaded_file is not None:
+    st.write("Selected file:", uploaded_file.name)
+
+
+#=============================================================================
+# Topics & Questions Section
+#=============================================================================
+st.subheader("Most Frequent Topics")
+# df = pd.DataFrame({
+#   'first column': [1, 2, 3, 4],
+#   'second column': [10, 20, 30, 40]
+# })
+
+# df
